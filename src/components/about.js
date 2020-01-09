ccc4222f90193985a5d80d8736b16e97f43db37f
ncooney94 <nick.cooney@gmail.com>
2020-01-09 23:18:28 +1100

    fleshed out about section

--- src/components/about.js
+++ src/components/about.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import SkillList from './skills';
+import Nue from './nueSkills';
 
 import {Link} from 'gatsby';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const AboutLinks = styled.a`
-
+  display: block;
+  padding-top: 1rem;
+  font-size: 1.5rem;
+  margin-left: 1.5rem;
 `
+
 
 export default () => (
   <Container>
@@ -22,8 +28,24 @@ export default () => (
       I consistently aim to improve on my own ability and skills while maintaining effective communication with colleagues and clients. I take pride in delivering my best and being a reliable employee. I’m excited to see what new opportunities become available in Software Engineering as i do enjoy designing, but to deliver a finished, interactive product is my true passion.</p>
       </Col>
     </Row>
+    <hr/>
+    <h1>Skills</h1>
     <Row>
-      <h1>Skills</h1>
+    <Col md={4}>
+      <h4>Skills I know</h4>
+    </Col>
+    <Col md={8}>
+      <SkillList />
+    </Col>
+    </Row>
+    <br/>
+    <Row>
+    <Col md={4}>
+      <h4>Skills I'm Learning</h4>
+      </Col>
+      <Col md={8}>
+      <Nue />
+      </Col>
     </Row>
   </Container>
 )
